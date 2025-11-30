3d7e0f982c73dfa57c6881ff2c582500a1c06ab9
AlpKirca <alperennet18@gmail.com>
2025-11-30 13:43:12 +0300

    Basic if statement code

---
+++ java-projects/ExerciseWithIfandScanner.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;    //we import scanner from util package 
+
+
+public class ExerciseWithIfandScanner {
+ 
+
+public static void main(String[] args) {
+    
+Scanner keyboard = new Scanner(System.in); 
+
+/*Please get a number from the user.If the user input is 2025,display 
+<<best year ever!!>> message to the console*/
+
+// we inform the user to give number to us 
+System.out.println("Please write a integer number ");
+
+int n = keyboard.nextInt();
+
+if (n == 2025 ) {
+System.out.println("Best year ever !!");
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
